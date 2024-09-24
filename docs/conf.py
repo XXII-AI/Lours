5549197236631c56dcc423fcccddebe7ce3c6f4e
Clément <mail@clementpinard.fr>
2024-09-24 14:11:12 +0200

    Fix version switcher

--- docs/conf.py
+++ docs/conf.py
@@ -30,25 +30,19 @@ html_theme = "pydata_sphinx_theme"
 html_logo = "_static/logo.png"
 
 # Define the json_url for our version switcher.
-json_url = "https://lours.readthedocs.io/latest/_static/switcher.json"
+json_url = "https://lours.readthedocs.io/latest/_static/version_switcher.json"
 
 # Define the version we use for matching in the version switcher.
 version_match = os.environ.get("READTHEDOCS_VERSION")
 release = lours.__version__
-# If READTHEDOCS_VERSION doesn't exist, we're not on RTD
-# If it is an integer, we're in a PR build and the version isn't correct.
-# If it's "latest" → change to "dev" (that's what we want the switcher to call it)
-if not version_match or version_match.isdigit() or version_match == "latest":
-    # For local development, infer the version to match from the package.
-    if "dev" in release or "rc" in release:
-        version_match = "dev"
-        # We want to keep the relative reference if we are in dev mode
-        # but we want the whole url if we are effectively in a released version
-        json_url = "_static/switcher.json"
-    else:
-        version_match = f"v{release}"
+if version_match.isdigit():
+    version_match = ""
+elif version_match == "latest":
+    version_match = "dev"
 elif version_match == "stable":
-    version_match = f"v{release}"
+    version_match = release
+
+print(f"Version is {version_match}")
 
 html_theme_options = {
     "navbar_align": "left",
